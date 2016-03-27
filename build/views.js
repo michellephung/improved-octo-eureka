1dbfddb7312e647b36a0a13fa539acb3eb4c310f
michellephung <michellephung@gmail.com>
2016-03-26 21:45:49 -0400

    things still working

--- build/views.js
+++ build/views.js
@@ -1,29 +1,44 @@
 var App = React.createClass({
-  displayName: "App",
+	displayName: "App",
 
 
-  render: function () {
-    return React.createElement(StartPlayingWordInput, null);
-  }
+	render: function () {
+		return React.createElement(StartPlayingWordInput, null);
+	}
 
 });
 
 var StartPlayingWordInput = React.createClass({
-  displayName: "StartPlayingWordInput",
+	displayName: "StartPlayingWordInput",
 
-  getInitialState: function () {
-    return null;
-  },
-  handleClick: function () {
-    this.setState(function (state) {
-      return { clickCount: state.clickCount + 1 };
-    });
-  },
-  render: function () {
-    return React.createElement("input", {
-      onChange: this.handleChange,
-      placeholder: "Type a word" });
-  }
+	getInitialState: function () {
+		return null;
+	},
+	handleChange: function () {
+		this.setState(function (state) {
+			console.log("b");
+			return { showStartBtn: true };
+		});
+	},
+	render: function () {
+		return React.createElement("input", {
+			onChange: this.handleChange,
+			placeholder: "Type a word" });
+	}
+});
+
+var StartBtn = React.createClass({
+	displayName: "StartBtn",
+
+	handleClick: function () {
+		console.log("click");
+	},
+
+	render: function () {
+		return React.createElement("input", {
+			onClick: this.handleClick,
+			placeholder: "Start" });
+	}
 });
 
 ReactDOM.render(React.createElement(App, null), document.getElementById('app'));
--- scripts/views.jsx
+++ scripts/views.jsx
@@ -1,33 +1,65 @@
-var App = React.createClass({
-
-	render: function () {
-		return (
-			<StartPlayingWordInput />
-		);
+requirejs([
+  "store",
+  "action",
+  "dispatcher"
+],
+function(Store, Action, Dispatcher) {
+  
+  var App = React.createClass({
+    render: function () {
+      return (
+        <StartPlayingWordInput />
+        <StartBtn />
+      );
     }
 
-});
+  });
 
 
-var StartPlayingWordInput = React.createClass({
-    getInitialState: function () {
- 		return null;
-    },
-    handleClick: function () {
-      this.setState(function(state) {
-        return {clickCount: state.clickCount + 1};
-      });
+  var StartPlayingWordInput = React.createClass({
+    handleChange: function () {
+      Action.startWordTyping();
     },
     render: function () {
-		return (
-			<input 
-				onChange={this.handleChange} 
-				placeholder="Type a word" />
-		);
+      return (
+        <input 
+          onChange={this.handleChange} 
+          placeholder="Type a word" />
+      );
     }
- });
+  });
 
-ReactDOM.render(
-	<App />,
-	document.getElementById('app')
-);
+
+  var StartBtn = React.createClass({
+    startBtn: function () {
+      return (
+        <input type="submit" text="Start"/>
+      );
+    }
+
+    handleClick: function () {
+      console.log("click");
+    },
+
+    render: function () {
+        var startBtn;
+
+        if (this.props.showStartBtn) {
+          startBtn = this.startBtn();
+        }
+
+        return (
+          <input 
+            onClick={this.handleClick} 
+            placeholder="Start" />
+          {startBtn}
+        );
+    )}
+  });
+
+  ReactDOM.render(
+    <App />,
+    document.getElementById('app')
+  );
+
+});
